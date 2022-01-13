cd02fa8d214b2d80fcaa91f8137f8de415dd3ccc
Baconsizzle1738 <zhengdundun@gmail.com>
2022-01-13 08:57:01 -0600

    more stufdf

---
+++ Prometheus/Sprites/Composite/CompositeSprites.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+var body_spriteSheet = {
+	0:preload("res://Sprites/Composite/HumanComp.png")
+}
+var face_spriteSheet = {
+	0:preload("res://Sprites/Composite/HumanComp.png")
+}
+var toga_spriteSheet = {
+	0:preload("res://Sprites/Composite/HumanComp.png")
+}
+var eye_spriteSheet = {
+	0:preload("res://Sprites/Composite/HumanComp.png")
+}
+var hair_spriteSheet = {
+	0:preload("res://Sprites/Composite/HumanComp.png")
+}
--- Prometheus/customPlayer.gd
+++ Prometheus/customPlayer.gd
@@ -1,20 +1,21 @@
 extends Node2D
 
 onready var bodySprite = $CompositeSprites/body
-onready var faceSprite
-onready var togaSprite
-onready var eyeSprite
-onready var hairSprite
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var faceSprite = $CompositeSprites/face
+onready var togaSprite = $CompositeSprites/toga
+onready var eyeSprite = $CompositeSprites/eye
+onready var hairSprite = $CompositeSprites/hair
+
+const compositeSprites = preload("res://Sprites/Composite/CompositeSprites.gd")
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	bodySprite.texture = compositeSprites.body_spriteSheet[0]
+	faceSprite.texture = compositeSprites.face_spriteSheet[0]
+	togaSprite.texture = compositeSprites.toga_spriteSheet[0]
+	eyeSprite.texture = compositeSprites.eye_spriteSheet[0]
+	hairSprite.texture = compositeSprites.hair_spriteSheet[0]
+	
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
