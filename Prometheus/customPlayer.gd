7f25d1185c83d1425fb47c800967bb0fdb272fe8
Baconsizzle1738 <55448932+Baconsizzle1738@users.noreply.github.com>
2022-01-14 00:26:00 -0600

    Apparently there is an invalid index in the array but I'm pretty sure I made it cororectly

--- Prometheus/customPlayer.gd
+++ Prometheus/customPlayer.gd
@@ -6,16 +6,15 @@ onready var togaSprite = $CompositeSprites/toga
 onready var eyeSprite = $CompositeSprites/eye
 onready var hairSprite = $CompositeSprites/hair
 
-const compositeSprites = preload("res://Sprites/Composite/CompositeSprites.gd")
+const composite_Sprites = preload("res://Sprites/Composite/CompositeSprites.gd")
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	bodySprite.texture = compositeSprites.body_spriteSheet[0]
-	faceSprite.texture = compositeSprites.face_spriteSheet[0]
-	togaSprite.texture = compositeSprites.toga_spriteSheet[0]
-	eyeSprite.texture = compositeSprites.eye_spriteSheet[0]
-	hairSprite.texture = compositeSprites.hair_spriteSheet[0]
+	bodySprite.texture = composite_Sprites.body_spriteSheet[0]
+	faceSprite.texture = composite_Sprites.face_spriteSheet[0]
+	togaSprite.texture = composite_Sprites.toga_spriteSheet[0]
+	eyeSprite.texture = composite_Sprites.eye_spriteSheet[0]
+	hairSprite.texture = composite_Sprites.hair_spriteSheet[0]
 	
-	pass # Replace with function body.
